ccf1e83a40799c1e5c0cc10ffc3c7495b7b90e2e
René Schultz Madsen <rm@microting.dk>
2020-09-16 14:03:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-if [ ! -d "/var/www/microting/eform-service-itemsplanning-plugin" ]; then
+if [ ! -d "/var/www/microting/eform-service-items-group-planning-plugin" ]; then
   cd /var/www/microting
   su ubuntu -c \
-  "git clone https://github.com/microting/eform-service-itemsplanning-plugin.git -b stable"
+  "git clone https://github.com/microting/eform-service-items-group-planning-plugin.git -b stable"
 fi
 
-cd /var/www/microting/eform-service-itemsplanning-plugin
+cd /var/www/microting/eform-service-items-group-planning-plugin
 git pull
 su ubuntu -c \
-"dotnet restore ServiceItemsPlanningPlugin.sln"
+"dotnet restore ServiceItemsGroupPlanningPlugin.sln"
 
 echo "################## START GITVERSION ##################"
 export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
 echo $GITVERSION
 echo "################## END GITVERSION ##################"
 su ubuntu -c \
-"dotnet publish ServiceItemsPlanningPlugin.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
+"dotnet publish ServiceItemsGroupPlanningPlugin.sln -o out /p:Version=$GITVERSION --runtime linux-x64 --configuration Release"
 
 su ubuntu -c \
 "mkdir -p /var/www/microting/eform-debian-service/MicrotingService/out/Plugins/"
 
-if [ -d "/var/www/microting/eform-debian-service/MicrotingService/out/Plugins/ServiceItemsPlanningPlugin" ]; then
-	rm -fR /var/www/microting/eform-debian-service/MicrotingService/out/Plugins/ServiceItemsPlanningPlugin
-fi
+rm -fR /var/www/microting/eform-debian-service/MicrotingService/out/Plugins/ServiceItemsGroupPlanningPlugin
 
 su ubuntu -c \
-"cp -av /var/www/microting/eform-service-itemsplanning-plugin/ServiceItemsPlanningPlugin/out /var/www/microting/eform-debian-service/MicrotingService/out/Plugins/ServiceItemsPlanningPlugin"
+"cp -av /var/www/microting/eform-service-items-group-planning-plugin/out /var/www/microting/eform-debian-service/MicrotingService/out/Plugins/ServiceItemsGroupPlanningPlugin"
 /rabbitmqadmin declare queue name=eform-service-itemsplanning-plugin durable=true
