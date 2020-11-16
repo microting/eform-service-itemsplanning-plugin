205826d0c18242f07f385969e6522b7a75f100d9
René Schultz Madsen <rm@microting.com>
2020-11-16 20:39:40 +0000

    Fixing the project names.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -6,8 +6,8 @@ if (( "$GIT_STATUS" > 0 )); then
 	git pull
 	cd ServiceItemsPlanningPlugin
 
-	dotnet add ServiceItemsPlanningPlugin.csproj package Microting.eForm
-	dotnet add ServiceItemsPlanningPlugin.csproj package Microting.ItemsPlanningBase
+	dotnet add ServiceItemsGroupPlanningPlugin.csproj package Microting.eForm
+	dotnet add ServiceItemsGroupPlanningPlugin.csproj package Microting.ItemsGroupPlanningBase
 
 	EFORM_VERSION=`dotnet list package | grep 'Microting.eForm ' | cut -c64-71`
 	EFORM_PLUGINBASE_VERSION=`dotnet list package | grep 'Microting.ItemsPlanningBase' | cut -c64-71`
@@ -28,7 +28,7 @@ if (( "$GIT_STATUS" > 0 )); then
 		NEW_GIT_VERSION="v$MAJOR_VERSION.$MINOR_VERSION.$BUILD_VERSION"
 		git tag "$NEW_GIT_VERSION"
 		git push --tags
-		git push	
+		git push
 		echo "Updated Microting eForm to ${EFORM_VERSION} and pushed new version ${NEW_GIT_VERSION}"
 	fi
 else
